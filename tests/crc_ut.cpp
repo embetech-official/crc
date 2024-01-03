699fe2fa2fa8adeb5ac14c84121aed56cc0681bd
Jan Macheta <jan.macheta@embe.tech>
2024-01-03 13:05:47 +0100

    fix presets

---
+++ tests/crc_ut.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+#include "embetech/crc8.h"
+
+TEST(CRC, Placeholder) {
+    SUCCEED();
+}
--- tests/ring_buffer_ut.cpp
+++
@@ -1,6 +0,0 @@
-#include "gtest/gtest.h"
-#include "embetech/ring_buffer.h"
-
-TEST(RingBuffer, Placeholder) {
-    SUCCEED();
-}
