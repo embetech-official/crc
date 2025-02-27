14556448c568534ffff0ade1878f2fb8d6666ebe
Mateusz Kubaszek <mateusz.kubaszek@embe.tech>
2025-02-27 14:57:51 +0100

    set cmake installation and presets

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -1,21 +1,35 @@
 include(CMakePackageConfigHelpers)
 
-set(CMAKES_EXPORT_DIR cmake/crc)
+set(HELPERS_DIR ${CMAKE_CURRENT_LIST_DIR}/embenet-toolchains/cmake/install_helpers)
 
-write_basic_package_version_file(crc-config-version.cmake COMPATIBILITY SameMajorVersion)
+set(METADATA_DIR cmake)
+set(INCLUDE_DIR include)
+set(LIBRARY_DIR lib/${CMAKE_LIBRARY_ARCHITECTURE})
+
+set_target_properties(crc PROPERTIES EXPORT_NAME crc)
+
+write_basic_package_version_file(
+  crc-config-version.cmake COMPATIBILITY SameMajorVersion ARCH_INDEPENDENT
+)
 
 configure_package_config_file(
-  ${PROJECT_SOURCE_DIR}/cmake/crc-config.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/crc-config.cmake
-  INSTALL_DESTINATION ${CMAKES_EXPORT_DIR}
+  ${HELPERS_DIR}/component-config.template.cmake
+  ${CMAKE_CURRENT_BINARY_DIR}/crc-config.cmake INSTALL_DESTINATION ${METADATA_DIR}
 )
 
-include(GNUInstallDirs)
-install(TARGETS crc EXPORT crc_targets)
+file(COPY_FILE ${HELPERS_DIR}/prefix_path_inject.template.txt
+     ${CMAKE_CURRENT_BINARY_DIR}/CMakeLists.txt
+)
 
-install(DIRECTORY ${CMAKE_SOURCE_DIR}/src/include/embetech DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+install(TARGETS crc EXPORT crc_targets ARCHIVE DESTINATION ${LIBRARY_DIR})
+
+install(DIRECTORY src/include/embetech DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
 
 install(FILES ${CMAKE_CURRENT_BINARY_DIR}/crc-config.cmake
-              ${CMAKE_CURRENT_BINARY_DIR}/crc-config-version.cmake DESTINATION ${CMAKES_EXPORT_DIR}
+              ${CMAKE_CURRENT_BINARY_DIR}/crc-config-version.cmake
+        DESTINATION ${METADATA_DIR}
 )
 
-install(EXPORT crc_targets NAMESPACE embetech:: DESTINATION ${CMAKES_EXPORT_DIR})
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/CMakeLists.txt DESTINATION ${METADATA_DIR})
+
+install(EXPORT crc_targets DESTINATION ${METADATA_DIR})
