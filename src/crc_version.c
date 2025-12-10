935a100af06038f3f3dda453abe7d8064a48036b
Jan Macheta <jmacheta@users.noreply.github.com>
2025-12-10 20:08:00 +0100

    remove unused macro

--- src/crc_version.c
+++ src/crc_version.c
@@ -5,9 +5,6 @@
 #error "CRC_VERSION is not defined"
 #endif
 
-#define STR_HELPER(x) #x
-#define STR(x) STR_HELPER(x)
-
 static_assert(sizeof(CRC_VERSION) <= 64, "CRC_VERSION too long"); // NOLINT(readability-magic-numbers) - 64 bytes max
 
 char const *CRC_GetVersionString(void) { return (CRC_VERSION); }
