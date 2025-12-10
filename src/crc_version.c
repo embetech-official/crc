141b4706c39b8dbcacc66215ecadd04e00dcdcbd
Jan Macheta <2372096+jmacheta@users.noreply.github.com>
2025-12-10 19:29:52 +0100

    apply formatting

--- src/crc_version.c
+++ src/crc_version.c
@@ -8,7 +8,6 @@
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
 
-
 static_assert(sizeof(CRC_VERSION) <= 64, "CRC_VERSION too long"); // NOLINT(readability-magic-numbers) - 8 chars + NUL
 
 char const *CRC_GetVersionString(void) { return (CRC_VERSION); }
